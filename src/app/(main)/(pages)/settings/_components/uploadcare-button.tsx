205d5f3a90879f5942b443e98cf1bd833ae4a90b
FelipeMDantas <felipemdantas@terra.com.br>
2024-04-26 21:49:15 -0300

    Connection to Neon PostgreSQL established.

---
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { useRef } from "react";
+import * as LR from "@uploadcare/blocks";
+
+type Props = {
+  onUpload: (e: string) => any;
+};
+
+LR.registerBlocks(LR);
+
+const UploadCareButton = ({ onUpload }: Props) => {
+  const ctxProviderRef = useRef<
+    typeof LR.UploadCtxProvider.prototype & LR.UploadCtxProvider
+  >(null);
+
+  return (
+    <div>
+      <lr-config ctx-name="my-uploader" pubkey="a9428ff5ff90ae7a64eb" />
+
+      <lr-file-uploader-regular
+        ctx-name="my-uploader"
+        css-src={`https://cdn.jsdelivr.net/npm/@uploadcare/blocks@0.35.2/web/lr-file-uploader-regular.min.css`}
+      />
+
+      <lr-upload-ctx-provider ctx-name="my-uploader" ref={ctxProviderRef} />
+    </div>
+  );
+};
+
+export default UploadCareButton;
---
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -0,0 +1,35 @@
+import ProfileForm from "@/components/forms/profile-form";
+import ProfilePicture from "./_components/profile-picture";
+import { db } from "@/lib/db";
+
+type Props = {};
+
+const Settings = async (props: Props) => {
+  const removeProfileImage = async () => {
+    "use server";
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
+        <span>Settings</span>
+      </h1>
+      <div className="flex flex-col gap-10 p-6">
+        <div>
+          <h2 className="text-2xl font-bold">User Profile</h2>
+          <p className="text-base text-white/50">
+            Add or update your information
+          </p>
+        </div>
+        <ProfilePicture
+          onDelete={removeProfileImage}
+          // userImage={user?.profileImage || ""}
+          // onUpload={uploadProfileImage}
+        ></ProfilePicture>
+        <ProfileForm />
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
